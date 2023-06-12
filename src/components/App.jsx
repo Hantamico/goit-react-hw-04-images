0f2e153d7bab3cf3c1ee2e7e30dbf75abe820135
Kirill_Halahan <Galagan.k.a.01@gmail.com>
2023-06-12 17:18:36 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,77 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ToastContainer } from "react-toastify";
+import { ProgressBar } from 'react-loader-spinner'
+import fetchImagesByName from '../services/fetchImages'
+import Button from "components/Button/Button";
 import Searchbar from "./Searchbar/Searchbar";
 import ImageGallery from "./ImageGallery/ImageGallery";
 
+const statuses = {
+  IDLE: "idle",
+  PENDING: "pending",
+  REJECTET: "rejected",
+  RESOLVED: "resolved"}
 
 export default function App() {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('');
+  const [imagesList, setImagesList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [status, setStatus] = useState(statuses.IDLE);
 
-    function handleFormSubmit(query){
+  useEffect(() => {
+    if (!query) {
+        return;
+    }
+    setTimeout(() => {
+      fetchImagesByName(query, page)
+        .then(({ hits, totalHits }) => {
+          setImagesList(v => [...v, ...hits]);
+          setTotalHits(totalHits);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(setStatus(statuses.RESOLVED));
+    }, 250);
+    },[page, query]);
+
+  function handleFormSubmit(query) {
+    setImagesList([]);
+    setStatus(statuses.PENDING);
     setQuery(query)
+    setPage(1);
   };
+
+  function handleLoadMore() {
+        setPage(v => v + 1);
+    };
+
+  const totalPages = Math.floor(totalHits / 12);
   
-  return (
+  if (status === statuses.IDLE) {
+    return <Searchbar onSubmit={handleFormSubmit} />
+  };
+
+  if (status === statuses.PENDING) {
+    return (
       <>
         <Searchbar onSubmit={handleFormSubmit} />
-        <ImageGallery query={query} />
-        <ToastContainer/>
-      </>
+        <ProgressBar height="100" width="160" wrapperStyle={{ display: 'block', margin: '0 auto', }} />
+      </>);
+  };
+
+  
+
+  if (status === statuses.RESOLVED) {
+      return (
+    <>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {totalHits !==0 && <ImageGallery imagesList={imagesList} />}
+      {page < totalPages && <Button loadMore={handleLoadMore} />}
+      <ToastContainer />
+    </>
     )
+  }
 }
   
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,57 +1,12 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import {useState, useEffect } from "react";
-import { ProgressBar } from 'react-loader-spinner'
-import { toast } from 'react-toastify'
-import fetchImagesByName from '../../services/fetchImages'
+import {useState,} from "react";
 import ImageGalleryItem from "components/ImageGalleryItem/ImageGallryItem";
-import Button from "components/Button/Button";
 import Modal from "components/Modal/Modal";
-import css from './imageGallery.module.css'
+import css from './imageGallery.module.css';
 
 
-export default function ImageGallery({ query }) {
-    const [imagesList, setImagesList] = useState(null);
-    // eslint-disable-next-line no-unused-vars
-    const [error, setError] = useState(null);
-    const [status, setStatus] = useState('idle');
-    const [page, setPage] = useState(1);
-    const [totalHits, setTotalHits] = useState(0);
+export default function ImageGallery({ imagesList }) {
     const [showModal, setShowModal] = useState(false);
     const [largeImage, setLargeImage] = useState(null);
-
-    useEffect(() => {
-        if (!query) {
-            return;
-        }
-
-        setStatus('pending');
-        setPage(v => v + 1);
-        setTimeout(() => {
-            fetchImagesByName(query, page)
-                .then(({hits, totalHits}) => {
-                    setPage(v => v + 1);
-                    setImagesList(hits);
-                    setTotalHits(totalHits);
-                    setStatus("resolved");
-                })
-                .catch(error => {
-                    setError(error);
-                    setStatus("rejected");
-                })
-            }, 250);
-    }, [query]);
-
-    function handleLoadMore(){
-        fetchImagesByName(query, page)
-            .then(imagesList => {
-                setPage(v => v + 1);
-                setImagesList(v => [...v, ...imagesList.hits]);
-            })
-            .catch(error => {
-                    setError(error);
-                    setStatus("rejected");
-            })
-    };
 
     function toggleModal(e){
         setShowModal(!showModal);
@@ -62,32 +17,12 @@ export default function ImageGallery({ query }) {
         setLargeImage(imagesList.filter(image => Number(image.id) === Number(e.target.id)))
     }
 
-
-    if (status === "idle") {
-            return;
-        }
-
-        if (status === "pending") {
-            return <ProgressBar height="100" width="160" wrapperStyle={{ display: 'block', margin: '0 auto', }} />
-        }
-
-        if (totalHits === 0) {
-            return toast.error(`По вашему запросу, ${query}, ничего не найдено`)
-        }
-
-        if (status === "resolved") {
-            const totalPages = Math.floor(totalHits / 12)
-        
-            return (<div>
-                {showModal && <Modal onClose={toggleModal}>
-                    <img  src={largeImage[0].largeImageURL} alt={largeImage[0].tags } />
-                            </Modal>
-                }
-                <ul className={css.imageGallery} >
-                    <ImageGalleryItem imagesList={imagesList} onImageClick={onImageClick} />
-                </ul>
-                {page < totalPages && <Button loadMore={handleLoadMore} />}
-            </div>
-            )
-        }    
+    return (
+        <ul className={css.imageGallery}>
+            <ImageGalleryItem imagesList={imagesList} onImageClick={onImageClick} /> 
+            {showModal && <Modal onClose={toggleModal}>
+                <img src={largeImage[0].largeImageURL} alt={largeImage[0].tags} width="800px"/>
+            </Modal>}
+       </ul>
+    )
 };
